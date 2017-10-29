84c2b0349e47dfcb50b6bcb7f22772c5909405ac
juanstepanenko <juan.agustin.stepanenko@gmail.com>
2017-10-29 12:53:11 -0300

    Filtrado Empresas hecho falta modificar y eliminar correctamente

--- PagoAgilFrba/AbmEmpresa/FiltroEmpresa.Designer.cs
+++ PagoAgilFrba/AbmEmpresa/FiltroEmpresa.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace PagoAgilFrba.AbmEmpresa
+﻿using System.Windows.Forms;
+
+namespace PagoAgilFrba.AbmEmpresa
 {
     partial class FiltroEmpresa
     {
@@ -28,11 +30,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox_Cuit = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Nombre = new System.Windows.Forms.TextBox();
+            this.combo_Rubro = new System.Windows.Forms.ComboBox();
+            this.button_Buscar = new System.Windows.Forms.Button();
+            this.button_Limpiar = new System.Windows.Forms.Button();
+            this.button_Cancelar = new System.Windows.Forms.Button();
+            this.dataGridView_Empresa = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Empresa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox_Cuit);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox_Nombre);
+            this.groupBox1.Controls.Add(this.combo_Rubro);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(416, 123);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtro de busqueda";
+            // 
+            // textBox_Cuit
+            // 
+            this.textBox_Cuit.Location = new System.Drawing.Point(104, 23);
+            this.textBox_Cuit.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Cuit.Name = "textBox_Cuit";
+            this.textBox_Cuit.Size = new System.Drawing.Size(304, 22);
+            this.textBox_Cuit.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 26);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Cuit";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre";
+            // 
+            // textBox_Nombre
+            // 
+            this.textBox_Nombre.Location = new System.Drawing.Point(104, 50);
+            this.textBox_Nombre.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Nombre.Name = "textBox_Nombre";
+            this.textBox_Nombre.Size = new System.Drawing.Size(304, 22);
+            this.textBox_Nombre.TabIndex = 0;
+            // 
+            // combo_Rubro
+            // 
+            this.combo_Rubro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.combo_Rubro.FormattingEnabled = true;
+            this.combo_Rubro.Location = new System.Drawing.Point(104, 80);
+            this.combo_Rubro.Margin = new System.Windows.Forms.Padding(4);
+            this.combo_Rubro.Name = "combo_Rubro";
+            this.combo_Rubro.Size = new System.Drawing.Size(304, 26);
+            this.combo_Rubro.TabIndex = 14;
+            this.combo_Rubro.SelectedIndexChanged += new System.EventHandler(this.combo_Rubro_SelectedIndexChanged);
+            // 
+            // button_Buscar
+            // 
+            this.button_Buscar.Location = new System.Drawing.Point(299, 146);
+            this.button_Buscar.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Buscar.Name = "button_Buscar";
+            this.button_Buscar.Size = new System.Drawing.Size(133, 37);
+            this.button_Buscar.TabIndex = 1;
+            this.button_Buscar.Text = "Buscar";
+            this.button_Buscar.UseVisualStyleBackColor = true;
+            this.button_Buscar.Click += new System.EventHandler(this.button_Buscar_Click);
+            // 
+            // button_Limpiar
+            // 
+            this.button_Limpiar.Location = new System.Drawing.Point(157, 146);
+            this.button_Limpiar.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Limpiar.Name = "button_Limpiar";
+            this.button_Limpiar.Size = new System.Drawing.Size(133, 37);
+            this.button_Limpiar.TabIndex = 2;
+            this.button_Limpiar.Text = "Limpiar";
+            this.button_Limpiar.UseVisualStyleBackColor = true;
+            this.button_Limpiar.Click += new System.EventHandler(this.button_Limpiar_Click);
+            // 
+            // button_Cancelar
+            // 
+            this.button_Cancelar.Location = new System.Drawing.Point(16, 146);
+            this.button_Cancelar.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Cancelar.Name = "button_Cancelar";
+            this.button_Cancelar.Size = new System.Drawing.Size(133, 37);
+            this.button_Cancelar.TabIndex = 3;
+            this.button_Cancelar.Text = "Cancelar";
+            this.button_Cancelar.UseVisualStyleBackColor = true;
+            this.button_Cancelar.Click += new System.EventHandler(this.button_Cancelar_Click);
+            // 
+            // dataGridView_Empresa
+            // 
+            this.dataGridView_Empresa.AllowUserToAddRows = false;
+            this.dataGridView_Empresa.AllowUserToDeleteRows = false;
+            this.dataGridView_Empresa.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView_Empresa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Empresa.Location = new System.Drawing.Point(16, 191);
+            this.dataGridView_Empresa.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView_Empresa.Name = "dataGridView_Empresa";
+            this.dataGridView_Empresa.RowTemplate.ReadOnly = true;
+            this.dataGridView_Empresa.Size = new System.Drawing.Size(1424, 245);
+            this.dataGridView_Empresa.TabIndex = 4;
+            this.dataGridView_Empresa.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_Empresa_CellClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 85);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Rubro";
+            // 
+            // FiltroEmpresa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1456, 450);
+            this.Controls.Add(this.dataGridView_Empresa);
+            this.Controls.Add(this.button_Cancelar);
+            this.Controls.Add(this.button_Limpiar);
+            this.Controls.Add(this.button_Buscar);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FiltroEmpresa";
+            this.Text = "Filtro de Empresas";
+            this.Load += new System.EventHandler(this.FiltroEmpresa_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Empresa)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_Cuit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        
+        private System.Windows.Forms.ComboBox combo_Rubro;
+        private System.Windows.Forms.TextBox textBox_Nombre;
+        private System.Windows.Forms.Button button_Buscar;
+        private System.Windows.Forms.Button button_Limpiar;
+        private System.Windows.Forms.Button button_Cancelar;
+        private System.Windows.Forms.DataGridView dataGridView_Empresa;
+        private Label label2;
+
     }
 }
