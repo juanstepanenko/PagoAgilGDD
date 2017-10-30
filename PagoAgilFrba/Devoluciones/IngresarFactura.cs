da33a98d561ed4d873ead00c570461a4237eea9e
juanstepanenko <juan.agustin.stepanenko@gmail.com>
2017-10-30 12:19:55 -0300

    agrego funcionalidad devolucion y ventanas hechas

---
+++ PagoAgilFrba/Devoluciones/IngresarFactura.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PagoAgilFrba.Devoluciones
+{
+    public partial class IngresarFactura : Form
+    {
+        public IngresarFactura()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // agregar chequeo de facturas y usuarios
+            this.Hide();
+            new Devolucion().ShowDialog();
+            this.Close();
+        }
+    }
+}
